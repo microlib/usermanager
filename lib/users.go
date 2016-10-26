ee7ba005db9868242757fb7c09d4f542b97dfb60
Marco Troisi <hello@marcotroisi.com>
2016-10-26 09:22:39 +0100

    USM-2 setup base integration tests + users

--- lib/users.go
+++ lib/users.go
@@ -1,12 +1,17 @@
 package usermanager
 
 type UsersInterface interface {
-	Find() []*UserInterface
-	FindOne() *UserInterface
-	Create()
-	Update()
-	Delete()
+	Find(params map[string]string) []*UserInterface
+	Get(id string) *UserInterface
+	Create(params map[string]string)
+	Update(id string, params map[string]string)
+	Delete(id string)
 }
 
 type Users struct {
+	users []map[string]string
+}
+
+func (u *Users) Find(params map[string]string) []*UserInterface {
+
 }
