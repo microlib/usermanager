ad7dc9c961455fdca7620353ac82682671574deb
Marco Troisi <hello@marcotroisi.com>
2016-10-27 08:38:33 +0100

    USM-2 implementing find method in users (WIP)

--- lib/users.go
+++ lib/users.go
@@ -1,8 +1,8 @@
 package usermanager
 
 type UsersInterface interface {
-	Find(params map[string]string) []*UserInterface
-	Get(id string) *UserInterface
+	Find(params map[string]string) ([]*UserInterface, error)
+	Get(id string) (*UserInterface, error)
 	Create(params map[string]string)
 	Update(id string, params map[string]string)
 	Delete(id string)
@@ -12,6 +12,10 @@ type Users struct {
 	users []map[string]string
 }
 
-func (u *Users) Find(params map[string]string) []*UserInterface {
+func (u *Users) Find(params map[string]string) ([]*UserInterface, error) {
+	result := []*User{}
+	user := NewUser("1", "Padraig", "padraig@ireland.ie", "123safe!")
+	result = append(result, user)
 
+	return result, nil
 }
--- lib/users_test.go
+++ lib/users_test.go
@@ -1,1 +1,36 @@
 package usermanager
+
+import (
+    "testing"
+)
+
+func TestUsers_Find(t *testing.T) {
+    //testCases := []struct {
+    //    params map[string]string
+    //    key    string
+    //    value  string
+    //}{
+    //    {map[string]string{"hello": "test"}, "hell", "Value not found"},
+    //    {map[string]string{"hello": "test"}, "hi", "Value not found"},
+    //}
+    //for _, tc := range testCases {
+    //    t.Run(fmt.Sprintf("value for key '%s'", tc.key), func(t *testing.T) {
+    usersMap := []map[string]string{
+        map[string]string{
+            "id": "1",
+            "name": "Padraig",
+            "email": "padraig@ireland.ie",
+            "password": "123safe!",
+        },
+    }
+    users := Users{usersMap}
+    search := map[string]string{"name": "Padraig"}
+    expected := []*User{}
+    user := NewUser("1", "Padraig", "padraig@ireland.ie", "123safe!")
+    expected = append(expected, user)
+    if res, _ := users.Find(search); res != expected {
+        t.Errorf("got %s; want %s", res, expected)
+    }
+    //    })
+    //}
+}
