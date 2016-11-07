c81a184f8bec8d583f81c8ddec07120ec48c7a3f
Marco Troisi <hello@marcotroisi.com>
2016-11-07 10:49:26 +0000

    USM-2 implementing find method in users + tests

--- lib/users.go
+++ lib/users.go
@@ -13,20 +13,43 @@ type Users struct {
 }
 
 func (u *Users) Find(params map[string]string) ([]UserInterface, error) {
-	result := []*User{}
+
+    result := map[int]*User{}
+
+    for uk, uv := range u.users {                // users
+        for pk, pv := range params {            // params
+            _, isUserInResult := result[uk]
+            if _, vv := uv[pk]; vv == pv {      // if user's param is right value
+
+                // if value is not in "result", add it
+				if !isUserInResult {
+                    result[uk] = uv
+                }
+                break
+            } else {
+                // here if value is in "result", remove it
+                if isUserInResult {
+                    delete(result, uk)
+                }
+            }
+
+
+        }
+    }
+
 	user := NewUser("1", "Padraig", "padraig@ireland.ie", "123safe!")
 	result = append(result, user)
 
 	userInterfaces := make([]UserInterface, len(result))
-    for i, d := range result {
-        userInterfaces[i] = d
-    }
+	for i, d := range result {
+		userInterfaces[i] = d
+	}
 
 	return userInterfaces, nil
 }
 
 func (u *Users) Get(id string) (UserInterface, error) {
-    return &User{}, nil
+	return &User{}, nil
 }
 
 func (u *Users) Create(params map[string]string) {
