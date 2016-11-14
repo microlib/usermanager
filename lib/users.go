051259dbec0ab21ae278f3ec813827b294628fdc
Marco Troisi <hello@marcotroisi.com>
2016-11-14 08:33:10 +0000

    USM-2 implemented Create

--- lib/users.go
+++ lib/users.go
@@ -15,28 +15,27 @@ type Users struct {
 // Find returns all the users that satisfy the values passed in the map
 func (u *Users) Find(params map[string]string) ([]UserInterface, error) {
 
-    result := map[int]*User{}
+	result := map[int]*User{}
 
-    for uk, uv := range u.users {               // users
-        for pk, pv := range params {            // params/values we're looking for
-            _, isUserInResult := result[uk]
-            if vv, _ := uv[pk]; vv == pv {      // if user's param is the value we're looking for
+	for uk, uv := range u.users { // users
+		for pk, pv := range params { // params/values we're looking for
+			_, isUserInResult := result[uk]
+			if vv, _ := uv[pk]; vv == pv { // if user's param is the value we're looking for
 
-                // if user is not in "result", add it
+				// if user is not in "result", add it
 				if !isUserInResult {
-                    result[uk] = NewUser(uv["id"], uv["name"], uv["email"], uv["password"])
-                }
-                break
-            } else {
-                // here if user is in "result", remove it
-                if isUserInResult {
-                    delete(result, uk)
-                }
-            }
+					result[uk] = NewUser(uv["id"], uv["name"], uv["email"], uv["password"])
+				}
+				break
+			} else {
+				// here if user is in "result", remove it
+				if isUserInResult {
+					delete(result, uk)
+				}
+			}
 
-
-        }
-    }
+		}
+	}
 
 	userInterfaces := make([]UserInterface, len(result))
 	for i, d := range result {
@@ -46,7 +45,6 @@ func (u *Users) Find(params map[string]string) ([]UserInterface, error) {
 	return userInterfaces, nil
 }
 
-
 // Get returns a single user by ID
 func (u *Users) Get(id string) (UserInterface, error) {
 	return NewUser("1", "Padraig", "padraig@irish.ie", "123abc"), nil
@@ -54,7 +52,12 @@ func (u *Users) Get(id string) (UserInterface, error) {
 
 // Create adds a new user
 func (u *Users) Create(params map[string]string) {
-
+	append(u.users, map[string]string{
+		"id":       params["id"],
+		"name":     params["name"],
+		"email":    params["email"],
+		"password": params["password"],
+	})
 }
 
 // Update modifies values of an existing user
